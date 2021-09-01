36abbc7059f4097af408a275922787d3639162c4
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2021-09-01 16:30:27 +0200

    commit script

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,19 @@
+//connect it to html element.
 const button = document.querySelector(`.container button`);
 const jokeText = document.querySelector(`.container p`);
 document.addEventListener(`DOMContentLoaded`,getJoke)
-
+ //Add click button
 button.addEventListener('click', getJoke);
-
+//fectch url
  async function getJoke()  {
  const jokeData = await fetch(`https://icanhazdadjoke.com/ `,{
      headers:{
         "Accept": "application/json"
      }
  });
+ //await json
  const jokeObj = await jokeData.json();
  jokeText.innerHTML = jokeObj.joke;
-
 
 }
 
